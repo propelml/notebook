f452ad08768834e6db8520a4fdffb1d181033d65
Parsa Ghadimi <qti3eqti3e@gmail.com>
2018-05-18 11:19:09 +0430

    Implement Propel router and some fixes in components

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { h, render, rerender } from "preact";
+import { App } from "./app";
 import { drainExecuteQueue } from "./components/cell";
 import { enableFirebase } from "./db";
-import { Router } from "./pages";
 import { assert, IS_WEB } from "./util";
 
 assert(IS_WEB);
@@ -9,7 +9,7 @@ assert(IS_WEB);
 enableFirebase();
 
 window.addEventListener("load", async () => {
-  render(<Router />, document.body, document.body.children[0]);
+  render(<App />, document.body, document.body.children[0]);
   await drainExecuteQueue();
 
   // If we're in a testing environment...
